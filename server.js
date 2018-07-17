b2f0d70d61d424177146cb5cdc30974e0ce739ad
Graham Cottridge <codinggc@gmail.com>
2018-07-17 01:59:27 +0100

    Update for registering users

--- server.js
+++ server.js
@@ -34,15 +34,9 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-  // bcrypt.compare("jelly", hash, function(err, res) {
-  //   // res == true
-  // });
-  // bcrypt.compare("veggies", hash, function(err, res) {
-  //   // res = false
-  // });
   if (req.body.email === database.users[0].email
     && req.body.password === database.users[0].password) {
-    res.json('success');
+    res.json(database.users[0]);
   } else {
     res.status(400).json('error logging in');
   }
@@ -58,7 +52,6 @@ app.post('/register', (req, res) => {
     id: '125',
     name: name,
     email: email,
-    password: password,
     entries: 0,
     joined: new Date()
   })
