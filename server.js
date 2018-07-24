f9f1185e174cc2c1e5abe203af390adfaa99accb
Graham Cottridge <codinggc@gmail.com>
2018-07-24 16:13:46 +0100

    Add db

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-animate-28101',
     user : 'gcottridge',
     password : '',
     database : 'react-face-recognition'
